8856052f1fb756eb159bd6c6e232e043217f68c4
gitoak <thorinthepro@gmail.com>
2022-02-05 08:25:24 +0100

    removed old nest server logic

--- libs/communication/src/lib/Communication.spec.ts
+++
@@ -1,7 +0,0 @@
-import { communication } from './Communication';
-
-describe('communication', () => {
-  it('should work', () => {
-    expect(communication()).toEqual('communication');
-  });
-});
--- libs/communication/src/lib/Communication.ts
+++
@@ -1,3 +0,0 @@
-export function communication(): string {
-  return 'communication';
-}
